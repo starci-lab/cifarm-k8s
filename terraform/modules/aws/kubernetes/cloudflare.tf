c005663eb58caaaa24dc967846bf2f9082ff2220
starci183 <cuongnvtse160875@gmail.com>
2024-12-20 15:44:35 +0700

    4

--- terraform/modules/aws/kubernetes/cloudflare.tf
+++ terraform/modules/aws/kubernetes/cloudflare.tf
@@ -8,18 +8,20 @@ data "cloudflare_zone" "zone" {
 }
 
 locals {
-  ns_records = [for ns in data.aws_route53_zone.zone.name_servers : {
-    name    = var.subdomain_prefix
-    content = ns
-  }]
+  # expect 4 name servers
+  ns_records = [
+    { name = var.subdomain_prefix, content = data.aws_route53_zone.zone.name_servers[0] },
+    { name = var.subdomain_prefix, content = data.aws_route53_zone.zone.name_servers[1] },
+    { name = var.subdomain_prefix, content = data.aws_route53_zone.zone.name_servers[2] },
+    { name = var.subdomain_prefix, content = data.aws_route53_zone.zone.name_servers[3] }
+  ]
 }
 
 # Create the NS records in Cloudflare for each name server
 resource "cloudflare_record" "ns" {
-  for_each = { for idx, ns in local.ns_records : "${var.subdomain_prefix}-${idx}" => ns }
-
-  name    = each.value.name
-  type    = "NS"
-  content = each.value.content
-  zone_id = data.cloudflare_zone.zone.id
+  count    = length(local.ns_records)  # Using count based on the length of the ns_records list (which is 4)
+  name     = local.ns_records[count.index].name  # Accessing the name via count.index
+  type     = "NS"
+  content  = local.ns_records[count.index].content  # Accessing the content via count.index
+  zone_id  = data.cloudflare_zone.zone.id
 }
